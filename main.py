122f6066a4b4ed880178013147740527274f7b69
Michele Gazzea <michelegazzea@hotmail.it>
2023-10-23 17:45:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
-import GridEyeS_lib as ges
-import numpy as np
+import VIRASS as ges
 import geopandas
-import pandas
 import matplotlib.pyplot as plt
 
-from skimage import morphology
-
-   
-
-# Load SAT image
-SAT_map, meta_data = ges.io.open_geoTiFF("_data/WorldView_area.tif")
-
-# Load tree Segmentation mask
-tree_mask = ges.io.open_geoTiFF("_data/trees/tree_segmentation/tree_mask_bn_1_2.tif", with_meta_data = False)   
 
 
-#%% SCAN power line
-
-power_line = ges.io.load_infrastructure_line("_data/power_lines/Kraftnett_files/power_lines.gpkg")
-# Convert to a more suitable CRS
-power_line = power_line.to_crs("epsg:32632")
-
-# Scan power line
-points_along_line = ges.infrastructure_utils.scan_lines(power_line, distance = 40, save_output = False)
+if not ("SAT_map" in locals()):
+    # Load SAT image
+    SAT_map, meta_data = ges.io.open_geoTiFF("_data/WorldView_area.tif")
+    
+    # Load tree Segmentation mask
+    tree_mask = ges.io.open_geoTiFF("_data/trees/tree_segmentation/tree_mask_bn_1_2.tif", with_meta_data = False)   
+    
+    
+    #%% SCAN power line
+    
+    power_line = ges.io.load_infrastructure_line("_data/power_lines/Kraftnett_files/power_lines.gpkg")
+    power_line = ges.infrastructure_utils.clean_line(power_line)
+    
+    # Convert to a more suitable CRS
+    power_line = power_line.to_crs("epsg:32632")
+    # power_line.to_file("_data/power_lines/Kraftnett_files/power_lines.gpkg", driver = "GPKG")
+        
+    # Create corridor
+    corridor = ges.infrastructure_utils.create_corridor(power_line, corridor_size = 40)
 
 
 #%% Tree inventory generation
+    
 
-radius = 40
-crowns = ges.infrastructure_utils.generate_tree_inventory(points_along_line, SAT_map, meta_data, radius, tree_mask,
-                                                          mode = "multiscale")
-
-
-#%% Add tree species and height
-
-crowns = ges.tree_utils.add_tree_species(crowns, tree_species_map_file = "_data/trees/tree_species/R_refined_1_Q.tif")
-crowns = ges.tree_utils.add_tree_height(crowns, nDSM = "_data/trees/3D_modeling/GT/warping/nDSM_warped.tif")
-
-#%% Export it
-
-crowns.to_file("_data/trees/crowns/crowns_246_w_extra_data.gpkg", driver="GPKG") 
+tree_inventory = ges.infrastructure_utils.generate_tree_inventory_along_power_lines(power_line, 
+                            satellite_map_file = "_data/WorldView_area.tif", 
+                            tree_mask_file = "_data/trees/tree_segmentation/tree_mask_bn_1_2.tif",
+                            large_corridor_side_size = 30, small_corridor_side_size = 10,
+                            tree_species_map_file = "_data/trees/tree_species/R_refined_2_Q.tif", 
+                            nDSM_map_file = "_data/trees/3D_modeling/nDSM_pred.tif", 
+                            mode = "multiscale")    
 
 
+tree_inventory.to_file("_data/trees/crowns/tmp.gpkg", driver = "GPKG")    
+# tree_inventory = geopandas.read_file("_data/trees/crowns/tree_inventory")
 
+        
 
+        
+            
 
-
-
+            
+            
 
 
 
