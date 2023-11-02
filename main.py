46873eecc459674e5ed8520890396dfd2c908790
Michele Gazzea <michelegazzea@hotmail.it>
2023-11-02 15:51:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,42 +3,43 @@ import geopandas
 import matplotlib.pyplot as plt
 
 
+    
+    
+# --Load power line
+power_line = ges.io.load_infrastructure_line("_data/power_lines/Kraftnett_files/power_lines.gpkg", crs = 32632)
+power_line = ges.infrastructure_utils.clean_line(power_line)
+# power_line.to_file("_data/power_lines/Kraftnett_files/power_lines.gpkg", driver = "GPKG")
+    
+# Create corridor
+# corridor = ges.infrastructure_utils.create_corridor(power_line, corridor_size = 30)
 
-if not ("SAT_map" in locals()):
-    # Load SAT image
-    SAT_map, meta_data = ges.io.open_geoTiFF("_data/WorldView_area.tif")
-    
-    # Load tree Segmentation mask
-    tree_mask = ges.io.open_geoTiFF("_data/trees/tree_segmentation/tree_mask_bn_1_2.tif", with_meta_data = False)   
-    
-    
-    #%% SCAN power line
-    
-    power_line = ges.io.load_infrastructure_line("_data/power_lines/Kraftnett_files/power_lines.gpkg")
-    power_line = ges.infrastructure_utils.clean_line(power_line)
-    
-    # Convert to a more suitable CRS
-    power_line = power_line.to_crs("epsg:32632")
-    # power_line.to_file("_data/power_lines/Kraftnett_files/power_lines.gpkg", driver = "GPKG")
-        
-    # Create corridor
-    corridor = ges.infrastructure_utils.create_corridor(power_line, corridor_size = 40)
-
-
-#%% Tree inventory generation
-    
-
+# Tree inventory generation
 tree_inventory = ges.infrastructure_utils.generate_tree_inventory_along_power_lines(power_line, 
                             satellite_map_file = "_data/WorldView_area.tif", 
-                            tree_mask_file = "_data/trees/tree_segmentation/tree_mask_bn_1_2.tif",
+                            tree_mask_file = "_data/trees/tree_segmentation/tree_pred_bn_Unet_attention_segmenter_1_4.tif",
                             large_corridor_side_size = 30, small_corridor_side_size = 10,
                             tree_species_map_file = "_data/trees/tree_species/R_refined_2_Q.tif", 
                             nDSM_map_file = "_data/trees/3D_modeling/nDSM_pred.tif", 
                             mode = "multiscale")    
 
 
-tree_inventory.to_file("_data/trees/crowns/tmp.gpkg", driver = "GPKG")    
-# tree_inventory = geopandas.read_file("_data/trees/crowns/tree_inventory")
+tree_inventory.to_file("_data/trees/crowns/tree_inventory.gpkg", driver = "GPKG")    
+# tree_inventory = geopandas.read_file("_data/trees/crowns/tree_inventory.gpkg")
+
+
+
+# Static risk map
+static_risk_map = ges.infrastructure_utils.static_risk_map(tree_inventory, power_line_height = 10.8, margin = 2)
+static_risk_map.to_file("_data/trees/crowns/static_risk.gpkg", driver = "GPKG")
+
+
+# Dynamic risk map  
+dynamic_risk_map = ges.infrastructure_utils.dynamic_risk_map(tree_inventory, power_line_height = 10.8, margin = 2, 
+                                                         wind_direction = 'W',
+                                                         wind_gust_speed = 17.4,
+                                                         power_line = power_line)
+dynamic_risk_map.to_file("_data/trees/crowns/dynamic_risk.gpkg", driver = "GPKG")
+ 
 
         
 
